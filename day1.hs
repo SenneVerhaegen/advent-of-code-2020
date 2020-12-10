f82c2e37b64ca8f694648e65132104f5166626e3
SenneVerhaegen <senne.verhaegen@outlook.com>
2020-12-10 21:45:31 +0100

    Refactor

--- day1.hs
+++ day1.hs
@@ -1,9 +1,11 @@
 -- Finds n entries in a list that sum to a target value
-findEntries :: Int -> Int -> [Int] -> Maybe [Int]
-findEntries target 0 (x:xs)    = findEntries target x xs
-findEntries target part (x:xs)
+findEntries :: Int -> [Int] -> Maybe [Int]
+findEntries target (x:xs) = findEntriesInternal target x xs
+
+findEntriesInternal :: Int -> Int -> [Int] -> Maybe [Int]
+findEntriesInternal target part (x:xs)
     | length (x:xs) == 1 = if part + x == target then Just [part, x] else Nothing
-    | otherwise = if target - part `elem` (x:xs) then Just [part, target - part] else findEntries target x xs
+    | otherwise = if target - part `elem` (x:xs) then Just [part, target - part] else findEntriesInternal target x xs
 
 resultProblem1 :: Maybe [Int] -> Int
 resultProblem1 (Just entries) = product entries
