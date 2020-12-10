c785e38fae04352a49e3273389bd725903cd0ce8
SenneVerhaegen <senne.verhaegen@outlook.com>
2020-12-10 23:57:27 +0100

    Stuck

--- day1.hs
+++ day1.hs
@@ -3,15 +3,17 @@ findEntries :: Int -> Int -> [Int] -> Maybe [Int]
 findEntries n target (x:xs) = findEntriesInternal n target x xs
 
 findEntriesInternal :: Int -> Int -> Int -> [Int] -> Maybe [Int]
-findEntriesInternal n target part (x:xs)
+findEntriesInternal 2 target part (x:xs)
     | length (x:xs) == 1            = if part + x == target then Just [part, x] else Nothing
-    | length (x:xs) == 2            = Just [n, part]
-    | length (x:xs) > 2 && n == 2   = if target - part `elem` (x:xs) then Just [part, target - part] else findEntriesInternal n target x xs
-    | otherwise                     = cc part $ findEntriesInternal (n - 1) (target - x) x xs
+    | length (x:xs) >= 2            = if target - part `elem` (x:xs) then Just [part, target - part] else findEntriesInternal 2 target x xs
+findEntriesInternal n target part (x:xs)
+    -- | length (x:xs) > 2             = case findEntriesInternal (n - 1) (target - part) x xs of
+    --         Nothing -> findEntriesInternal n (target - x) (head xs) (tail xs)
+    --         result -> cc part result
+    -- | otherwise = Nothing
 
-resultProblem1 :: Maybe [Int] -> Int
-resultProblem1 (Just entries) = product entries
-resultProblem1 Nothing = error "No entries found"
+-- findEntriesInternal n target part (x:xs) = cc part $ findEntriesInternal (n - 1) (target - part) x xs
 
-cc :: Int -> Maybe [Int] -> Maybe [Int]
+-- Prepends an a to an optional list of a's
+cc :: a -> Maybe [a] -> Maybe [a]
 cc x list = Just . (:) x $ concat list
